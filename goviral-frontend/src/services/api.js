0d088727f5032a50dc4fdad11bf55dd7c2912ecf
Pritam Kundu <pritamkundu@Pritams-MacBook-Air.local>
2026-05-19 14:52:47 +0530

    frontend api setup

--- goviral-frontend/src/services/api.js
+++ goviral-frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
 });
 
 export default API;
